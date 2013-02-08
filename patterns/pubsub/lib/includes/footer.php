7dbfbe5ab069428cb47aad8001fb93bba3365c4a
Paul Barrick <paul@imperativedesign.net>
2013-02-07 22:55:17 -0500

    changes

--- patterns/pubsub/lib/includes/footer.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-<script type="text/javascript" src="js/src/global.js" charset="utf-8"></script>
-</body>
-</html>
